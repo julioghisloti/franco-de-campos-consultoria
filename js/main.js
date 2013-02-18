797ee0d51114617b19c579d27351474809043518
Júlio Ghisloti <juliohisloti@gmail.com>
2013-02-18 19:37:10 -0300

    Alteração de fontes, cores e indentidade visual

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+	// Medidas =====================================================================================
+	$(window).load(medidas);
+	$(window).resize(medidas);
+
+	function medidas() {
+
+		var altura = document.documentElement.clientHeight; //Pega o Haight
+		var largura = document.documentElement.clientWidth //Paga o Width
+
+
+		if (altura < 650){
+
+			var altura = 650;
+			$("#top-all").show().css({'height': altura+"px"}); //Top -->
+
+		}else{
+
+			$("#top-all").show().css({'height': altura+"px"}); //Top -->
+
+		}
+
+
+		if (largura < 1300){
+
+			var altura = 850
+
+		}else{
+
+			//Nada		
+
+		}
+
+		
+		$(".box-content-all").show().css({'height': altura+"px"}); //Empresa -->
+
+	 }
+});
