9794a067611ae4db3130d2ed3404762b1ff59b84
Júlio Ghisloti <juliohisloti@gmail.com>
2013-02-20 09:54:11 -0300

    Franco de Campos Consultoria
    Correção script de loading

--- js/main.js
+++ js/main.js
@@ -1,39 +1,40 @@
 $(document).ready(function() {
 
-	// Medidas =====================================================================================
-	$(window).load(medidas);
-	$(window).resize(medidas);
+	  // Medidas ===================================================================================
+	  $(window).load(medidas);
+	  $(window).resize(medidas);
 
-	function medidas() {
+	  function medidas() {
 
-		var altura = document.documentElement.clientHeight; //Pega o Haight
-		var largura = document.documentElement.clientWidth //Paga o Width
+	    var altura = document.documentElement.clientHeight; //Pega o Haight
+	    var largura = document.documentElement.clientWidth //Paga o Width
+
+	    if (altura < 650) {
+
+	      var altura = 650;
+	      $("#top-all").show().css({
+	        'height': altura + "px"
+	      }); //Top -->
+	    } else {
+
+	      $("#top-all").show().css({
+	        'height': altura + "px"
+	      }); //Top -->
+	    }
 
 
-		if (altura < 650){
+	    if (largura < 1300) {
 
-			var altura = 650;
-			$("#top-all").show().css({'height': altura+"px"}); //Top -->
+	      var altura = 850
 
-		}else{
+	    } else {
 
-			$("#top-all").show().css({'height': altura+"px"}); //Top -->
-
-		}
+	      //Nada		
+	    }
 
 
-		if (largura < 1300){
-
-			var altura = 850
-
-		}else{
-
-			//Nada		
-
-		}
-
-		
-		$(".box-content-all").show().css({'height': altura+"px"}); //Empresa -->
-
-	 }
+	    $(".box-content-all").show().css({
+	      'height': altura + "px"
+	    }); //Empresa -->
+	  }
 });
